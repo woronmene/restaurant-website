e6007503c2fe9413169fd2d49ffc0f99ba31e72d
Woron Mene <meneworon4@gmail.com>
2024-01-04 20:14:04 +0100

    centralise hero text

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,6 +3,6 @@
         <div class="w-[300px] md:w-[400px] lg:w-[600px]">
             <img class="w-full" src="/logo_white.png" alt="">
         </div>
-        <p class="tracking-3 text-white  text-sm lg:text-lg">Exceptional Nigerian Experience on the go!</p>
+        <p class="tracking-3 text-white text-sm lg:text-lg text-center">Exceptional Nigerian Experience on the go!</p>
     </main>
 </section>
